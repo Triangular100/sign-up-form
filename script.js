4b9271215acfe13fafee72feaaf4f73cef0cb63f
Triangular100 <139834502+Triangular100@users.noreply.github.com>
2023-08-23 22:26:00 -0500

    Add password validation

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log("Hey there!");
+function validatePassword() {
+    if (passwordInput.value === "" || confirmInput.value === "") {
+        passwordLabel.classList.remove("password-no-match");
+        passwordInput.classList.remove("password-no-match");
+        confirmInput.classList.remove("password-no-match");
+        return false;
+    }
+    if (passwordInput.value === confirmInput.value) {
+        passwordLabel.classList.remove("password-no-match");
+        passwordInput.classList.remove("password-no-match");
+        confirmInput.classList.remove("password-no-match");
+        return true;
+    }
+
+    passwordLabel.classList.add("password-no-match");
+    passwordInput.classList.add("password-no-match");
+    confirmInput.classList.add("password-no-match");
+    return false;
+
+}
+
+function validateForm(e) {
+    if (!validatePassword()) {
+        passwordInput.focus();
+        e.preventDefault();
+    }
+}
+
+const passwordLabel = document.querySelector("label[for=password");
+const passwordInput = document.querySelector("input[id=password]");
+const confirmInput = document.querySelector("input[id=confirm_password]");
+const form = document.querySelector("form");
+
+passwordInput.addEventListener("focusout", validatePassword);
+confirmInput.addEventListener("focusout", validatePassword);
+form.addEventListener("submit", validateForm);
